24bde2641cd46056f8678d86b6df790b5598b95f
Nathan Villaescusa <nathan@transloc.com>
2011-01-19 18:30:05 +0000

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = '0.2'
 
-setup(name='Geographic coordinate system',
-      version=version,
-      description="Geographic Coordinate System types and functions",
-      long_description="",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='TransLoc',
-      author_email='nathan@transloc.com',
-      url='http://transloc.com',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-        'shapely',        
-      ]
+setup(name                  = 'GCS',
+      version               = version,
+      description           = "Geographic Coordinate System types and functions",      
+      author                = 'TransLoc',
+      author_email          = 'nathan@transloc.com',
+      url                   = 'http://transloc.com',
+      license               = 'All rights reserved',
+      packages              = find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data  = False,
+      zip_safe              = True,
+      install_requires      = open('requirements.txt').readlines()
       )
